c0a21e5fcd740dfbab9c6af6d6a0a61f13f99a57
Ramadhani Ally <hectorally94@gmail.com>
2022-01-28 13:20:45 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/com/prospect/madosevaluationapp/ui/Login.java
    #	app/src/main/res/layout/activity_login.xml

--- app/src/main/java/com/prospect/madosevaluationapp/ui/UseUi/UserRegisration.java
+++ app/src/main/java/com/prospect/madosevaluationapp/ui/UseUi/UserRegisration.java
@@ -2,9 +2,13 @@ package com.prospect.madosevaluationapp.ui.UseUi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.prospect.madosevaluationapp.R;
+import com.prospect.madosevaluationapp.ui.Login;
 
 public class UserRegisration extends AppCompatActivity {
 
@@ -12,5 +16,15 @@ public class UserRegisration extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_regisration);
+       Button register = findViewById(R.id.btnregister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentResident = new Intent(UserRegisration.this, Login.class);
+                intentResident.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+                startActivity(intentResident);
+                finish();            }
+        });
+
     }
 }
